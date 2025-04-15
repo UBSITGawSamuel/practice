19037ee6ec1904b776cb7c793f8eeaefa476116f
UBSITGawSamuel <20246829@s.ubaguio.edu>
2025-04-15 21:03:53 +0800

    logout feature update

--- project/script.js
+++ project/script.js
@@ -12,17 +12,14 @@
                 const username = document.getElementById("username").value.trim();
                 const password = document.getElementById("password").value.trim();
         
-                // Dummy login check
+               
                 if (username === "admin" && password === "1234") {
                     errorMsg.style.display = "none";
-                    // ✅ Redirect to your page (replace 'dashboard.html' with your page)
+                    
                     window.location.href = "index.html";
                 } else {
                     errorMsg.style.display = "block";
                 }
             });
         });
-
-
-
-        
+      
